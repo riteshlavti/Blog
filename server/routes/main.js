0cd45b43d28d3ad38daa35e9c53c2c963d77c627
shaktimaan29 <riteshlavti@gmail.com>
2023-09-22 19:11:43 +0530

    new changes

--- server/routes/main.js
+++ server/routes/main.js
@@ -153,4 +153,12 @@ router.get('/about',(req,res)=>{
     });
 });
 
+
+router.get('/contact',(req,res)=>{
+    res.render('contact',{
+        currentRoute: '/contact'
+    });
+});
+
+
 module.exports = router
